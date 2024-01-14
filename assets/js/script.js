98b99f0f46ec05e198f41ec25253eed73061d910
Zoey Ai <aizoey@umich.edu>
2024-01-14 14:04:44 -0500

    js change

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,12 +139,14 @@ for (let i = 0; i < formInputs.length; i++) {
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
-
+console.log(navigationLinks);
+console.log(pages);
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
-
+    console.log('Clicked!');
     for (let i = 0; i < pages.length; i++) {
+      console.log(this.innerHTML.toLowerCase(), pages[i].dataset.page);
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
